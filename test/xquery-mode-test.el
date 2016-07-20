02ba761feb7f278829a623ac825ccf096772c889
Artem Malyshev <proofit404@gmail.com>
2016-07-20 10:11:42 +0300

    Add ert tests.

---
+++ test/xquery-mode-test.el
@@ -0,0 +1,13 @@
+;;; xquery-mode-test.el --- xquery-mode test suite
+
+;;; Commentary:
+
+;;; Code:
+
+(ert-deftest test-xquery-mode ()
+  "Test `xquery-mode'."
+  (should nil))
+
+(provide 'xquery-mode-test)
+
+;;; xquery-mode-test.el ends here
