33f72a516d44ddbf086eab4c5a1ad7a41c5caf3e
Artem Malyshev <proofit404@gmail.com>
2016-08-08 20:05:39 +0300

    Add define-indentation-test macro.

--- test/test-helper.el
+++ test/test-helper.el
@@ -5,12 +5,28 @@
 ;;; Code:
 
 (require 'cask)
+(require 'shut-up)
 
 (let ((source-directory (locate-dominating-file load-file-name "Cask")))
   (cask-initialize source-directory)
   (add-to-list 'load-path source-directory))
 
 (undercover "xquery-mode.el")
+
+(require 'xquery-mode)
+
+(defmacro define-indent-test (testname args doc before after)
+  "Generate `ert' test for indentation."
+  (declare (indent 2))
+  `(ert-deftest ,testname ,args
+     ,doc
+     (with-current-buffer (generate-new-buffer "*fixture*")
+       (insert ,before)
+       (goto-char (point-min))
+       (xquery-mode)
+       (shut-up (indent-region (point-min) (point-max)))
+       (should (string= (buffer-substring-no-properties (point-min) (point-max))
+                        ,after)))))
 
 (provide 'test-helper)
 
