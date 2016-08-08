fbc73327d02f5b4e732b731c65a2c7dfba77770a
Artem Malyshev <proofit404@gmail.com>
2016-08-08 20:05:49 +0300

    Add some indentation tests.

--- test/xquery-mode-test.el
+++ test/xquery-mode-test.el
@@ -4,9 +4,37 @@
 
 ;;; Code:
 
-(ert-deftest test-xquery-mode ()
-  "Test `xquery-mode'."
-  (should t))
+(define-indent-test function-body-first-line ()
+  "Indent first line of function body."
+  "
+declare function local:get-provider-group-membership($mpf-provider as element(provider))
+{
+				(for $group-membership in $mpf-provider/provider-group-membership
+" "
+declare function local:get-provider-group-membership($mpf-provider as element(provider))
+{
+  (for $group-membership in $mpf-provider/provider-group-membership
+")
+
+(define-indent-test flwor-expression-in-brackets ()
+  "Indent FLWOR excression within square brackets."
+  "
+(for $group-membership in $mpf-provider/provider-group-membership
+				order by $group-membership/group-membership-effective-date descending
+" "
+(for $group-membership in $mpf-provider/provider-group-membership
+ order by $group-membership/group-membership-effective-date descending
+ ")
+
+(define-indent-test flwor-expression-keyword-on-next-line ()
+  "Indent line started with RETURN keyword to same column as previous line started with ORDER BY."
+  "
+								order by $group-membership/group-membership-effective-date descending
+								return $group-membership)[1]
+" "
+order by $group-membership/group-membership-effective-date descending
+return $group-membership)[1]
+")
 
 (provide 'xquery-mode-test)
 
