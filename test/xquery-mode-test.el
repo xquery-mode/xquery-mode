e54fe099dc9460ea5157efc634ed68aeb2000597
Artem Malyshev <proofit404@gmail.com>
2016-08-29 22:54:40 +0300

    Support multiline indentation inside round and curly brackets.

--- test/xquery-mode-test.el
+++ test/xquery-mode-test.el
@@ -192,6 +192,50 @@ return
   <tr>
 ")
 
+(define-indent-test "xquery version \"1.0-ml\";
+(: dump.xqy :)
+declare namespace bk = \"http://www.marklogic.com/ns/gs-books\";
+
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head>
+<title>Database dump</title>
+</head>
+<body>
+<b>XML Content</b>
+{
+for $book in doc(\"books.xml\")/bk:books/bk:book
+return
+<pre>
+Title: { $book/bk:title/text() }
+Author: { ($book/bk:author/bk:first/text(), \" \",
+$book/bk:author/bk:last/text()) }
+Publisher: { $book/bk:publisher/text() }
+</pre>
+}
+</body>
+</html>" "xquery version \"1.0-ml\";
+(: dump.xqy :)
+declare namespace bk = \"http://www.marklogic.com/ns/gs-books\";
+
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+  <head>
+    <title>Database dump</title>
+  </head>
+  <body>
+  <b>XML Content</b>
+    {
+      for $book in doc(\"books.xml\")/bk:books/bk:book
+      return
+        <pre>
+          Title: { $book/bk:title/text() }
+          Author: { ($book/bk:author/bk:first/text(), \" \",
+                     $book/bk:author/bk:last/text()) }
+          Publisher: { $book/bk:publisher/text() }
+        </pre>
+    }
+  </body>
+</html>")
+
 (provide 'xquery-mode-test)
 
 ;;; xquery-mode-test.el ends here
