9497b6e0e3f7bbe185d02b2a3cde2d97fe8d5b50
Artem Malyshev <proofit404@gmail.com>
2016-09-18 20:17:09 +0300

    Reindent complex test.

--- test/xquery-mode-test.el
+++ test/xquery-mode-test.el
@@ -388,21 +388,21 @@ declare function local:validate-with-tpi($tpi as xs:string, $claim as element())
 };
 
 <claim-validations>
-{
-  for $claim in subsequence(/c:claim, 1, 500)
-  let $tpi := $claim/c:access-point/c:medicaid-id/text()
-  let $errors := if ($tpi) then
-                   local:validate-with-tpi($tpi, $claim)
-                 else
-                   ()
-  return
-    if ($errors) then
-    <cv:claim id=\"{$claim/@c:id}\">
-      { for $message in $errors return <cv:error>{ $message }</cv:error> }
-    </cv:claim>
-    else
-      ()
-}
+  {
+    for $claim in subsequence(/c:claim, 1, 500)
+    let $tpi := $claim/c:access-point/c:medicaid-id/text()
+    let $errors := if ($tpi) then
+                     local:validate-with-tpi($tpi, $claim)
+                   else
+                     ()
+    return
+      if ($errors) then
+        <cv:claim id=\"{$claim/@c:id}\">
+          { for $message in $errors return <cv:error>{ $message }</cv:error> }
+        </cv:claim>
+      else
+        ()
+  }
 </claim-validations>
 ")
 
