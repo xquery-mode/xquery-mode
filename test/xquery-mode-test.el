61f35805032d7df25c51fff556e55863e807c050
Artem Malyshev <proofit404@gmail.com>
2016-09-18 20:08:55 +0300

    Indent nested if properly.

--- test/xquery-mode-test.el
+++ test/xquery-mode-test.el
@@ -476,6 +476,36 @@ if ($facility-type = \"GROUP\") then
   )
 ")
 
+(define-indent-test "
+if ($facility-type = \"GROUP\") then
+(
+if ($mpf-group-entry/provider-tax-id/irs-number != $facility/c:ein) then
+\"billing provider tax ID does not match EIN in MPF\"
+else
+()
+)
+else
+(
+if ($mpf-entry/npi-api/npi-api != $provider/c:npi) then
+\"rendering provider NPI does not match MPF npi-api record\"
+else
+())
+" "
+if ($facility-type = \"GROUP\") then
+  (
+   if ($mpf-group-entry/provider-tax-id/irs-number != $facility/c:ein) then
+     \"billing provider tax ID does not match EIN in MPF\"
+   else
+     ()
+  )
+else
+  (
+   if ($mpf-entry/npi-api/npi-api != $provider/c:npi) then
+     \"rendering provider NPI does not match MPF npi-api record\"
+   else
+     ())
+")
+
 (provide 'xquery-mode-test)
 
 ;;; xquery-mode-test.el ends here
