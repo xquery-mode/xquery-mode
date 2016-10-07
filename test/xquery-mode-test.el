8f50fcffe3dd7583ac1e30d5f3f048a4acfcfb7c
Artem Malyshev <proofit404@gmail.com>
2016-10-07 22:57:57 +0300

    Mutate fixture once.

--- test/xquery-mode-test.el
+++ test/xquery-mode-test.el
@@ -11,33 +11,26 @@
 (setq-default indent-tabs-mode nil)
 (setq xquery-mode-indent-style 'native)
 
-(defun unindent ()
-  (while (re-search-forward "^ +" nil t)
-    (replace-match "")))
-
-(defun indent-with-tabs ()
-  (while (re-search-forward "^ +" nil t)
-    (replace-match (make-string (random 10) ?\t))))
-
-(defun indent-with-spaces ()
-  (while (re-search-forward "^ +" nil t)
-    (replace-match (make-string (random 20) ? ))))
-
 (dolist (xqy (f-glob "*.xqy" (and load-file-name (f-dirname load-file-name))))
-  (cl-loop for mutation from 1
-           for mutate in '(unindent indent-with-tabs indent-with-spaces)
-           do (let ((num (f-base xqy))
-                    (fixture (f-read xqy)))
-                (eval
-                 `(ert-deftest ,(intern (format "test-xquery-mode-indent-%s-%d" num mutation)) ()
-                    (with-current-buffer (generate-new-buffer (format "*fixture-%s-%d*" ,num ,mutation))
-                      (insert ,fixture)
-                      (goto-char (point-min))
-                      (,mutate)
-                      (xquery-mode)
-                      (indent-region (point-min) (point-max))
-                      (should (string= (buffer-substring-no-properties (point-min) (point-max))
-                                       ,fixture))))))))
+  (let ((num (f-base xqy))
+        (fixture (f-read xqy)))
+    (eval
+     `(ert-deftest ,(intern (format "test-xquery-mode-indent-%s" num)) ()
+        (with-current-buffer (generate-new-buffer (format "*fixture-%s*" ,num))
+          (insert ,fixture)
+          (goto-char (point-min))
+          (while (re-search-forward "^ *" nil t)
+            (cl-case (random 4)
+              (0 (replace-match ""))
+              (1 (replace-match (make-string (random 10) ?\t)))
+              (2 (replace-match (make-string (random 20) ? )))
+              (3 (replace-match (concat
+                                 (cl-loop for i to (random 20)
+                                          collect (if (zerop (random 2)) ? ?\t)))))))
+          (xquery-mode)
+          (indent-region (point-min) (point-max))
+          (should (string= (buffer-substring-no-properties (point-min) (point-max))
+                           ,fixture)))))))
 
 (provide 'xquery-mode-test)
 
