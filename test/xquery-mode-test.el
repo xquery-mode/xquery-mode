d4a0b6c7a6ed764a067358f1de902a1bf02b5a9d
Artem Malyshev <proofit404@gmail.com>
2016-10-07 16:59:20 +0300

    Mutate fixtures during tests.

--- test/xquery-mode-test.el
+++ test/xquery-mode-test.el
@@ -4,24 +4,40 @@
 
 ;;; Code:
 
+(require 'cl-lib)
 (require 'f)
 (require 'xquery-mode)
 
 (setq-default indent-tabs-mode nil)
 (setq xquery-mode-indent-style 'native)
 
+(defun unindent ()
+  (while (re-search-forward "^ +" nil t)
+    (replace-match "")))
+
+(defun indent-with-tabs ()
+  (while (re-search-forward "^ +" nil t)
+    (replace-match (make-string (random 10) ?\t))))
+
+(defun indent-with-spaces ()
+  (while (re-search-forward "^ +" nil t)
+    (replace-match (make-string (random 20) ? ))))
+
 (dolist (xqy (f-glob "*.xqy" (and load-file-name (f-dirname load-file-name))))
-  (let ((num (f-base xqy))
-        (fixture (f-read xqy)))
-    (eval
-     `(ert-deftest ,(intern (format "test-xquery-mode-indent-%s" num)) ()
-        (with-current-buffer (generate-new-buffer (format "*fixture-%s*" ,num))
-          (insert ,fixture)
-          (goto-char (point-min))
-          (xquery-mode)
-          (indent-region (point-min) (point-max))
-          (should (string= (buffer-substring-no-properties (point-min) (point-max))
-                           ,fixture)))))))
+  (cl-loop for mutation from 1
+           for mutate in '(unindent indent-with-tabs indent-with-spaces)
+           do (let ((num (f-base xqy))
+                    (fixture (f-read xqy)))
+                (eval
+                 `(ert-deftest ,(intern (format "test-xquery-mode-indent-%s-%d" num mutation)) ()
+                    (with-current-buffer (generate-new-buffer (format "*fixture-%s-%d*" ,num ,mutation))
+                      (insert ,fixture)
+                      (goto-char (point-min))
+                      (,mutate)
+                      (xquery-mode)
+                      (indent-region (point-min) (point-max))
+                      (should (string= (buffer-substring-no-properties (point-min) (point-max))
+                                       ,fixture))))))))
 
 (provide 'xquery-mode-test)
 
