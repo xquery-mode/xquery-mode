db13425b5f84bb97adcbfa85f17adcd913218d43
Artem Malyshev <proofit404@gmail.com>
2016-11-28 03:15:40 +0300

    Use open XML tag start inside read table.
    Simple open XML tag doesn't exist any more.

--- xquery-mode.el
+++ xquery-mode.el
@@ -558,7 +558,7 @@ START and END are region boundaries."
                          close-quote-stmt word-stmt)
                        '(inside-xml-tag
                          cdata-start-stmt xml-comment-start-stmt open-curly-bracket-stmt
-                         self-closing-xml-tag-stmt open-xml-tag-stmt close-xml-tag-stmt)))
+                         self-closing-xml-tag-stmt open-xml-tag-start-stmt close-xml-tag-stmt)))
            ;; TODO: don't calculate indent pairs.  Write it declarative way.
            ;; TODO: make variable below calculated only.
            (non-pairs '(cdata-end-stmt
